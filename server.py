15724140efa69d1f5a5080116e569884aa6d7f8b
David Cullinan <davidcullinan@email.com>
2025-06-13 17:24:25 +1000

    Hello world basics initialised

---
+++ server.py
@@ -0,0 +1,12 @@
+import os
+from flask import Flask, send_from_directory
+
+app = Flask(__name__, static_folder='static')
+
+@app.route('/')
+def index():
+    return send_from_directory(app.static_folder, 'index.html')
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
