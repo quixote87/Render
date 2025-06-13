0526c2df45fc809596e6342fc39af3bbfad18554
David Cullinan <davidcullinan@email.com>
2025-06-13 18:30:20 +1000

    Had to build the npm package first

--- server.py
+++ server.py
@@ -7,6 +7,10 @@ app = Flask(__name__, static_folder='static')
 def index():
     return send_from_directory(app.static_folder, 'index.html')
 
+@app.route('/<path:path>')
+def static_proxy(path):
+    return send_from_directory(app.static_folder, path)
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 10000))
     app.run(host='0.0.0.0', port=port)
